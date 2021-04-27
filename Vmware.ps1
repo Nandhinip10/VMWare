7eb1807787e3fb69b3014c2df4d9c00bb7947803
Nandhinip10 <46020357+Nandhinip10@users.noreply.github.com>
2021-04-27 22:03:54 +0530

    Add files via upload

---
+++ Vmware.ps1
@@ -0,0 +1,14 @@
+﻿#To get the list of VM Names
+$VMnames = Get-VM | Select-Object | ForEach-Object {$_.Name}
+#Getting the VM Names alone to VMnames variable
+
+# Setting the New Vm names to each VM in VMnames variable
+ForEach ($i in $VMnames)
+
+{
+echo "Old VMname $i"
+$newname = Read-Host "New hostname"
+Get-VM $i |Set-VM -name $newname -Confirm:$false
+$ChangedVM = Get-VM $newname |  Select Name -ExpandProperty Name
+echo "VM Name has been successfully changed to $ChangedVM"
+}
